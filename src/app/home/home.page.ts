b1e3a8cf3c994d843a8f050408ef05d52e6cb627
unknown <benja.arevalo.onetto@gmail.com>
2024-09-06 11:43:08 -0400

    responsive

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,15 @@
 import { IonicModule } from '@ionic/angular';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import {ChangeDetectionStrategy, Component,signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component,signal, OnDestroy, inject} from '@angular/core';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {FormControl,FormsModule,ReactiveFormsModule,Validators} from '@angular/forms';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
-import {merge} from 'rxjs';
+import {merge, Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 /**
  * @title Input with a clear button
@@ -21,15 +23,46 @@ import {merge} from 'rxjs';
   imports: [IonicModule,MatFormFieldModule, MatSelectModule,MatInputModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatIconModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePage {
+export class HomePage implements OnDestroy{
   readonly email = new FormControl('', [Validators.required, Validators.email]);
 
   errorMessage = signal('');
+  destroyed = new Subject<void>();
+  currentScreenSize: string = ''; 
+
+  displayNameMap = new Map([
+    [Breakpoints.XSmall, 'XSmall'],
+    [Breakpoints.Small, 'Small'],
+    [Breakpoints.Medium, 'Medium'],
+    [Breakpoints.Large, 'Large'],
+    [Breakpoints.XLarge, 'XLarge'],
+  ]);
 
   constructor() {
     merge(this.email.statusChanges, this.email.valueChanges)
       .pipe(takeUntilDestroyed())
       .subscribe(() => this.updateErrorMessage());
+
+      inject(BreakpointObserver)
+      .observe([
+        Breakpoints.XSmall,
+        Breakpoints.Small,
+        Breakpoints.Medium,
+        Breakpoints.Large,
+        Breakpoints.XLarge,
+      ])
+      .pipe(takeUntil(this.destroyed))
+      .subscribe(result => {
+        for (const query of Object.keys(result.breakpoints)) {
+          if (result.breakpoints[query]) {
+            this.currentScreenSize = this.displayNameMap.get(query) ?? 'Unknown';
+          }
+        }
+      });
+  }
+  ngOnDestroy() {
+    this.destroyed.next();
+    this.destroyed.complete();
   }
 
   updateErrorMessage() {

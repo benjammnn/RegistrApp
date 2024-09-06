23528c24b75e6e6b225d2c8a0a1033542e630aff
benjammnn <benja.arevalo.onetto@gmail.com>
2024-09-05 21:07:27 -0400

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,13 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule,BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatInputModule ,IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,BrowserAnimationsModule, MatFormFieldModule, MatInputModule ,IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+/**
+ * @title Input with a clear button
+ */
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonicModule,MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatIconModule],
 })
 export class HomePage {
+
+  value = 'Clear me'
 
   constructor() {}
 

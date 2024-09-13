2ab935d96deda866130d3a62dfdaf57691d405f9
benjammnn <benja.arevalo.onetto@gmail.com>
2024-09-12 21:58:06 -0300

    Home actualizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,34 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, model } from '@angular/core';
+import {CdkMenu, CdkMenuItem,  CdkMenuTrigger} from '@angular/cdk/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 import { Router } from '@angular/router';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 import {IonicModule} from  '@ionic/angular';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {DatePipe} from '@angular/common';
+import {MatListModule} from '@angular/material/list';
 
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
+export interface Section {
+  name: string;
+  updated: Date;
 }
 
 /**
- * @title Dynamic grid-list
+ * @title 
  */
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone : true,
-  imports: [MatGridListModule, IonicModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatGridListModule, MatCardModule, MatDatepickerModule, MatButtonModule, 
+  IonicModule, CdkMenu, CdkMenuItem, CdkMenuTrigger,  MatDividerModule, MatIconModule, MatListModule,  DatePipe],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePage {
-  tiles: Tile[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-  ];
-
   user: any;
+  selected = model<Date | null>(null);
+  folders: Section[] = [
+    {
+      name: 'Certificados',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'Semestres',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Asignaturas',
+      updated: new Date('1/28/16'),
+    },
+  ];
+  notes: Section[] = [
+    {
+      name: 'Horario',
+      updated: new Date('2/20/16'),
+    },
+    {
+      name: 'Evaluaciones pendientes',
+      updated: new Date('1/18/16'),
+    },
+  ];
 
   constructor(private router: Router) {
     const navigation = this.router.getCurrentNavigation();

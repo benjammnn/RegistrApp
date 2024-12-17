83d5073f95b55afd498a205e1dce7bcae91cd4e7
Vich0yarz <133251252+Vich0yarz@users.noreply.github.com>
2024-12-16 21:26:15 -0300

    canactivate login

---
+++ src/app/login/canactivate.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CanActivateGuard implements CanActivate { 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (this.authService.isLoggedIn()) {
+      this.router.navigate(['/home']);
+      return false;
+    }
+    return true;
+  }
+}
